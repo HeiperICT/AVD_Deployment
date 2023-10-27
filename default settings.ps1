7c491634a07770c5870a558f60aa2199fe2df9f4
HeiperICT <149155964+HeiperICT@users.noreply.github.com>
2023-10-27 22:59:14 +0200

    Add files via upload

--- default settings.ps1
+++ default settings.ps1
@@ -57,18 +57,9 @@ function signoutbutton {
     Copy-Item "C:\Deployment\Default settings\Afmelden.lnk" "C:\Users\Public\Desktop\Afmelden.lnk"
 }
 
-function defaultapps {
-    winget install -e -h --id VideoLAN.VLC
-    winget install -e -h --id 7zip.7zip
-    winget install -e -h --id Google.Chrome
-    winget install -e -h --id Adobe.Acrobat.Reader.64-bit
-    winget install -e -h --id Notepad++.Notepad++
-}
-
 AddDeploymentTree
 OutlookSignIn
 frxtrayshortcut
 ExperienceIndicator
 paping
 signoutbutton
-defaultapps
