e79266735baf4c4adae5477bd23e44a7d68e21bc
HeiperICT <149155964+HeiperICT@users.noreply.github.com>
2023-10-27 11:40:08 +0200

    Add files via upload

---
+++ Clean_WinX.ps1
@@ -0,0 +1,59 @@
+﻿Write-Host "Checking for elevated permissions..."
+
+#$script = $PSScriptRoot+"\clean_winx.ps1"
+
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+[Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "Insufficient permissions to run this script. Opening the script as Administrator."
+    Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Unrestricted -File "C:\deployment\Winx\clean_winx.ps1"' -Verb RunAs
+}else{
+    Write-Host "Code is running as administrator — go on executing the script..." -ForegroundColor Green
+    pause
+
+    $TempPath=$PSScriptRoot+"\temp"
+
+    Write-Host "Checking if $TempPath exists" -ForegroundColor Green
+
+    $folderexist = test-path $TempPath
+    if($folderexist = "False"){
+        New-Item -Path $PSScriptRoot -Name "temp" -ItemType "directory"
+    }
+    
+    
+    
+    
+    
+    $WinXPath="C:\Users\Default\AppData\Local\Microsoft\Windows\WinX"
+    $TempPath=$PSScriptRoot+"\temp"
+
+    
+
+    Write-Host "Create back-up of important shortcuts" -ForegroundColor Green
+        
+    #Save
+    Copy-Item "$WinXPath\Group2\3 - Windows Explorer.lnk" -Destination "$TempPath\3 - Windows Explorer.lnk"
+    Copy-Item "$WinXPath\Group2\5 - Task Manager.lnk" -Destination "$TempPath\5 - Task Manager.lnk"
+    
+    
+
+
+    Write-Host "Removing all items from WinX menu" -ForegroundColor Green
+    #Remove
+    Remove-Item "$WinXPath\Group2\*.*"
+    Remove-Item "$WinXPath\Group3\*.*"
+
+    
+
+    Write-Host "Restoring important items to WinX menu" -ForegroundColor Green
+    #Restore
+    Copy-Item "$TempPath\3 - Windows Explorer.lnk" -Destination "$WinXPath\Group2\3 - Windows Explorer.lnk"
+    Copy-Item "$TempPath\5 - Task Manager.lnk" -Destination "$WinXPath\Group2\5 - Task Manager.lnk"
+
+       
+
+    Write-Host "Cleaning up back-up" -ForegroundColor Green
+
+    Remove-Item $TempPath -Recurse
+    
+    
+}
---
+++ default settings.ps1
@@ -0,0 +1,57 @@
+Set-ExecutionPolicy -ExecutionPolicy Bypass
+
+function OutlookSignIn {
+    Add-AppxPackage -Register "C:\Windows\SystemApps\Microsoft.AAD.BrokerPlugin_cw5n1h2txyewy\Appxmanifest.xml" -DisableDevelopmentMode
+
+}
+
+function frxtrayshortcut {
+    $TargetFile = "C:\Program Files\FSLogix\Apps\frxtray.exe"
+    $ShortcutFile = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\frxtray.lnk"
+    $WScriptShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+    $Shortcut.TargetPath = $TargetFile
+    $Shortcut.Save()    
+}
+
+
+#deze aanpassen incl download
+function ExperienceIndicator {
+    # Download actie
+    $url = "https://heiperavddeployment.blob.core.windows.net/deploymentprd/Experience Indicator/ExperienceIndicator.exe"
+    $dest = "C:\Deployment\Experience indicator\ExperienceIndicator.exe"
+    Invoke-WebRequest -Uri $url -OutFile $dest
+    # Snelkoppeling
+    $TargetFile = "C:\Deployment\Experience indicator\ExperienceIndicator.exe"
+    $ShortcutFile = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\ExperienceIndicator.lnk"
+    $WScriptShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+    $Shortcut.TargetPath = $TargetFile
+    $Shortcut.Save()
+}
+
+
+function paping {
+    # Download actie
+    $url = "https://heiperavddeployment.blob.core.windows.net/deploymentprd/Paping/paping.exe"
+    $dest = "C:\Deployment\Paping\paping.exe"
+    Invoke-WebRequest -Uri $url -OutFile $dest
+    # Kopier actie
+    Copy-Item "C:\Deployment\Paping\paping.exe" C:\Windows\System32\paping.exe
+}
+
+function signoutbutton {
+    # Download actie
+    $url = "https://heiperavddeployment.blob.core.windows.net/deploymentprd/SignOut/Afmelden.lnk"
+    $dest = "C:\Deployment\Default settings\Afmelden.lnk"
+    Invoke-WebRequest -Uri $url -OutFile $dest
+    # Kopier actie    
+    Copy-Item "C:\Deployment\Default settings\Afmelden.lnk" "C:\Users\Public\Desktop\Afmelden.lnk"
+}
+
+
+OutlookSignIn
+frxtrayshortcut
+ExperienceIndicator
+paping
+signoutbutton
