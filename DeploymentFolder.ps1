20138b021bef71c9c50e457d29be5c3aac746af5
HeiperICT <ondersteuning@heiper.nl>
2023-11-09 15:13:29 +0100

    deployment geoptimaliseerd

--- DeploymentFolder.ps1
+++ DeploymentFolder.ps1
@@ -1,8 +1,9 @@
 function AddDeploymentTree {
-    New-Item -Path "C:\" -Name "Deployment" -ItemType "directory"
-    New-Item -Path "C:\Deployment" -Name "Experience indicator" -ItemType "directory"
+    New-Item -Path "C:\Deployment" -Name "Experience indicator" -ItemType "directory" -Force
     New-Item -Path "C:\Deployment" -Name "Paping" -ItemType "directory"
     New-Item -Path "C:\Deployment" -Name "Default settings" -ItemType "directory"
     New-Item -Path "C:\Deployment" -Name "Microsoft office" -ItemType "directory"
 
 }
+
+AddDeploymentTree
