0311b404009d32bbc851f3dea7baf8f950fb5462
HeiperICT <149155964+HeiperICT@users.noreply.github.com>
2024-02-27 16:40:38 +0100

    Update default settings.ps1

--- default settings.ps1
+++ default settings.ps1
@@ -15,7 +15,7 @@ function ExperienceIndicator {
     # Download actie
     $url = "https://heiperavddeployment.blob.core.windows.net/deploymentprd/Experience Indicator/ExperienceIndicator.exe"
     $dest = "C:\Deployment\Experience indicator\ExperienceIndicator.exe"
-    Invoke-WebRequest -Uri $url -OutFile $dest
+    Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path $dest -Force )
     # Snelkoppeling
     $TargetFile = "C:\Deployment\Experience indicator\ExperienceIndicator.exe"
     $ShortcutFile = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\ExperienceIndicator.lnk"
@@ -30,7 +30,7 @@ function paping {
     # Download actie
     $url = "https://heiperavddeployment.blob.core.windows.net/deploymentprd/Paping/paping.exe"
     $dest = "C:\Deployment\Paping\paping.exe"
-    Invoke-WebRequest -Uri $url -OutFile $dest
+    Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path $dest -Force )
     # Kopier actie
     Copy-Item "C:\Deployment\Paping\paping.exe" C:\Windows\System32\paping.exe
 }
@@ -39,7 +39,7 @@ function signoutbutton {
     # Download actie
     $url = "https://heiperavddeployment.blob.core.windows.net/deploymentprd/SignOut/Afmelden.lnk"
     $dest = "C:\Deployment\Default settings\Afmelden.lnk"
-    Invoke-WebRequest -Uri $url -OutFile $dest
+    Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path $dest -Force )
     # Kopier actie    
     Copy-Item "C:\Deployment\Default settings\Afmelden.lnk" "C:\Users\Public\Desktop\Afmelden.lnk"
 }
